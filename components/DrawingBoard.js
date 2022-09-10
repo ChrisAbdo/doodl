3285f62aef02cedd907ffa95d20c07335e96c5ab
chris abdo <chris.j.abdo@gmail.com>
2022-09-09 20:29:49 -0400

    ADDED: POTD Modal added

--- components/DrawingBoard.js
+++ components/DrawingBoard.js
@@ -28,7 +28,7 @@ class DrawingBoard extends React.Component {
     this.setState({ color: color.rgb });
   };
 
-  strokewtf = (e) => {
+  strokewidth = (e) => {
     this.setState({ strokeWidth: e.target.value });
   };
 
@@ -42,7 +42,7 @@ class DrawingBoard extends React.Component {
     const styles = reactCSS({
       default: {
         color: {
-          width: "100px",
+          width: "100%",
           height: "14px",
           borderRadius: "2px",
           background: `rgba(${this.state.color.r}, ${this.state.color.g}, ${this.state.color.b}, ${this.state.color.a})`,
@@ -85,7 +85,7 @@ class DrawingBoard extends React.Component {
             <span className="text-black">Stroke width:</span>
             <input
               // console log every time the number is changed
-              onChange={(e) => this.strokewtf(e)}
+              onChange={(e) => this.strokewidth(e)}
               type="range"
               min="0"
               max="100"
@@ -97,6 +97,40 @@ class DrawingBoard extends React.Component {
           </div>
           <div className="rounded-none hover:bg-transparent">
             <div className="btn-group items-center justify-center">
+              <label
+                htmlFor="my-modal-6"
+                onClick={() => this.canvas.current.undo()}
+                className="btn btn-outline btn-ghost rounded-none"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z"
+                  />
+                </svg>
+              </label>
+              <input type="checkbox" id="my-modal-6" className="modal-toggle" />
+              <div className="modal modal-bottom sm:modal-middle">
+                <div className="modal-box">
+                  <h3 className="font-bold text-lg">prompt of the day</h3>
+                  <p className="py-4">
+                    THIS IS A PLACEHOLDER TEXT FOR A PROMPT OF THE DAY.
+                  </p>
+                  <div className="modal-action">
+                    <label htmlFor="my-modal-6" className="btn">
+                      DRAW!
+                    </label>
+                  </div>
+                </div>
+              </div>
               <button
                 onClick={() => this.canvas.current.undo()}
                 className="btn btn-outline btn-ghost rounded-none"
